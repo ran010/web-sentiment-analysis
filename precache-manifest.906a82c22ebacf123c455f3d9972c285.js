0dfa7b8b0da7fb45f08c8dd9ac3ef85551d454a2
ran010 <ranjanbajra@gmail.com>
2020-05-24 11:46:44 +0545

    Updates

---
+++ precache-manifest.906a82c22ebacf123c455f3d9972c285.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a7dd4b7337b76216945e0e2fb17151",
+    "url": "/tweet-sentiment-analysis/index.html"
+  },
+  {
+    "revision": "925aed16eb7181fe6d4f",
+    "url": "/tweet-sentiment-analysis/static/css/2.d61b5d69.chunk.css"
+  },
+  {
+    "revision": "796ec30fb8e7d9c640a8",
+    "url": "/tweet-sentiment-analysis/static/css/main.14b913c6.chunk.css"
+  },
+  {
+    "revision": "925aed16eb7181fe6d4f",
+    "url": "/tweet-sentiment-analysis/static/js/2.11a7be02.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tweet-sentiment-analysis/static/js/2.11a7be02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796ec30fb8e7d9c640a8",
+    "url": "/tweet-sentiment-analysis/static/js/main.4328157e.chunk.js"
+  },
+  {
+    "revision": "4694d0d31245c1661b15",
+    "url": "/tweet-sentiment-analysis/static/js/runtime-main.07126a18.js"
+  }
+]);
