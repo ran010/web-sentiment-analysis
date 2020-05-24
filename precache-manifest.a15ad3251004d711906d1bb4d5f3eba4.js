0a0636078c6c8996a2fe893a37c656fcb14eab6b
ran010 <ranjanbajra@gmail.com>
2020-05-24 12:05:18 +0545

    Updates

---
+++ precache-manifest.a15ad3251004d711906d1bb4d5f3eba4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031d0446d142e6b61ed154aa71bdcdd2",
+    "url": "/web-sentiment-analysis/index.html"
+  },
+  {
+    "revision": "925aed16eb7181fe6d4f",
+    "url": "/web-sentiment-analysis/static/css/2.d61b5d69.chunk.css"
+  },
+  {
+    "revision": "7dc8f98afb86a6ab027e",
+    "url": "/web-sentiment-analysis/static/css/main.14b913c6.chunk.css"
+  },
+  {
+    "revision": "925aed16eb7181fe6d4f",
+    "url": "/web-sentiment-analysis/static/js/2.11a7be02.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/web-sentiment-analysis/static/js/2.11a7be02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc8f98afb86a6ab027e",
+    "url": "/web-sentiment-analysis/static/js/main.881f1ab3.chunk.js"
+  },
+  {
+    "revision": "c82880e7dde8a7e1510f",
+    "url": "/web-sentiment-analysis/static/js/runtime-main.717a041d.js"
+  }
+]);
